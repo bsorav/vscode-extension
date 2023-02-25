d13cccfda186f6bf33c7e63a6a41c1835553ba59
Sorav Bansal <sbansal@iitd.ac.in>
2023-02-25 16:10:38 +0530

    product.js: Printing of proper node names

--- eqchecker/media/viewProof/scripts/product.js
+++ eqchecker/media/viewProof/scripts/product.js
@@ -38,27 +38,122 @@ function initializeContainer(){
     container.style.height = '100%';
 }
 
+function line_column_map_get_value_for_pc(proptree, p, key)
+{
+  //console.log("proptree =\n" + JSON.stringify(proptree));
+  const arr = proptree[key];
+  //console.log("key = " + key + ", arr =\n" + JSON.stringify(arr));
+  for (var i = 0; i < arr.length; i++) {
+    //console.log("d2=\n" + JSON.stringify(d2));
+    if (arr[i].pc === p) {
+      return arr[i].string;
+    }
+  }
+  return undefined;
+}
+
+function tfg_llvm_obtain_line_and_column_names_for_pc(src_tfg_llvm, src_pc)
+{
+  var src_linename, src_columnname, src_line_and_column_names;
+
+  const src_linename_map = src_tfg_llvm["linename_map"];
+  const src_columnname_map = src_tfg_llvm["columnname_map"];
+  const src_line_and_column_names_map = src_tfg_llvm["line_and_column_names_map"];
+
+  const src_index = src_pc.split('%')[0];
+  if (src_pc === 'L0%0%d') {
+    src_linename = 'entry';
+    src_columnname = "";
+    src_line_and_column_names = src_linename;
+  } else if (src_index.charAt(0) === 'L') {
+    var pc_components = src_pc.split('%');
+    pc_components[2] = "d";
+    const src_pc_default_subsubindex = pc_components.join('%');
+    src_linename = line_column_map_get_value_for_pc(src_linename_map, src_pc_default_subsubindex, "linename");
+    src_columnname = line_column_map_get_value_for_pc(src_columnname_map, src_pc_default_subsubindex, "columnname");
+    src_line_and_column_names = line_column_map_get_value_for_pc(src_line_and_column_names_map, src_pc_default_subsubindex, "line_and_column_names");
+    //if (src_linename === undefined) {
+    //  console.log(`src_pc_default_subsubindex = ${src_pc_default_subsubindex}\n`);
+    //  console.log("src_linename_map =\n");
+    //  for (let [key, value] of src_linename_map.entries()) {
+    //  	console.log(key + " = " + value)
+    //  }
+    //}
+  } else {
+    src_linename = 'exit';
+    src_columnname = "";
+    src_line_and_column_names = src_linename;
+  }
+  return [src_linename, src_columnname, src_line_and_column_names];
+}
+
+function tfg_asm_obtain_line_and_column_names_for_pc(dst_tfg_asm, dst_pc)
+{
+  var dst_linename, dst_columnname, dst_line_and_column_names;
+  const dst_index = dst_pc.split('%')[0];
+  if (dst_pc === 'L0%0%d') {
+    dst_linename = 'entry';
+    dst_columnname = "";
+    dst_line_and_column_names = dst_linename;
+  } else if (dst_index.charAt(0) === 'L') {
+    dst_linename = dst_index.substring(1);
+    dst_columnname = "0"
+    dst_line_and_column_names = dst_linename + "c0";
+  } else {
+    dst_linename = 'exit';
+    dst_columnname = "";
+    dst_line_and_column_names = dst_linename;
+  }
+  return [dst_linename, dst_columnname, dst_line_and_column_names];
+}
+
+function getNodesMap(nodes_in, src_tfg_llvm, dst_tfg_llvm, dst_tfg_asm)
+{
+  var nodeMap = {};
+  var idx = 0;
+  nodes_in.forEach(element => {
+    const src_pc = element.pc.split('_')[0];
+    const dst_pc = element.pc.split('_')[1];
+
+    const [src_linename, src_columnname, src_line_and_column_names] = tfg_llvm_obtain_line_and_column_names_for_pc(src_tfg_llvm, src_pc);
+
+    var dst_linename, dst_columnname, dst_line_and_column_names;
+    if (dst_tfg_llvm === undefined) {
+      [dst_linename, dst_columnname, dst_line_and_column_names] = tfg_asm_obtain_line_and_column_names_for_pc(dst_tfg_asm, dst_pc);
+    } else {
+      [dst_linename, dst_columnname, dst_line_and_column_names] = tfg_llvm_obtain_line_and_column_names_for_pc(dst_tfg_llvm, dst_pc);
+    }
+    console.log(`element.pc = ${element.pc}, src_pc = ${src_pc}, dst_pc = ${dst_pc}, src_linename = ${src_linename}, src_columnname = ${src_columnname}, src_line_and_column_names = ${src_line_and_column_names}, dst_linename = ${dst_linename}, dst_columnname = ${dst_columnname}, dst_line_and_column_names = ${dst_line_and_column_names}\n`);
+
+    const label = src_line_and_column_names + " ; " + dst_line_and_column_names;
+    nodeMap[element.pc] = {idx: idx, src_pc: src_pc, dst_pc: dst_pc, src_linename: src_linename, columnname: src_columnname, line_and_column_names: src_line_and_column_names, dst_linename: dst_linename, dst_columnname: dst_columnname, dst_line_and_column_names: dst_line_and_column_names, label: label};
+    idx++;
+  });
+  return nodeMap;
+}
+
 function drawNetwork(cfg) {
 
-    var nodeMap = {};
-    var idx = 0;
+    //var nodeMap = {};
+    //var idx = 0;
     //console.log(`drawNetwork: prod_cfg =\n${JSON.stringify(cfg)}\n`);
     const graph_hierarchy = cfg["graph-hierarchy"];
-    //console.log(`drawNetwork: graph_hierarchy=\n${JSON.stringify(graph_hierarchy)}\n`);
     const graph = graph_hierarchy["graph"];
-    //console.log(`drawNetwork: graph=\n${JSON.stringify(graph)}\n`);
     const nodes_in = graph["nodes"];
-    //console.log(`drawNetwork: nodes_in=\n${JSON.stringify(nodes_in)}\n`);
-    var edges_in = graph["edges"];
-    //console.log(`drawNetwork: edges_in=\n${JSON.stringify(edges_in)}\n`);
+    const edges_in = graph["edges"];
+    const corr_graph = graph_hierarchy["corr_graph"];
+    const src_tfg = corr_graph["src_tfg"];
+    const dst_tfg = corr_graph["dst_tfg"];
 
-    nodes_in.forEach(element => {
-        nodeMap[element.pc] = idx;
-        idx++;
-    });
+    const src_tfg_llvm = src_tfg["tfg_llvm"];
 
-    var nodes = new vis.DataSet(nodes_in.map(function(node, idx) {return {id:idx, label:node.pc/*node[0] + "," + node[1]*/};}));
-    var edges = new vis.DataSet(edges_in.map(function(edge) {return {from:nodeMap[edge.from_pc], to:nodeMap[edge.to_pc], label:""/*edge.path1 + '\n\n' +edge.path2*/};}));
+    const dst_tfg_llvm = dst_tfg["tfg_llvm"];
+    const dst_tfg_asm = dst_tfg["tfg_asm"];
+
+    const nodeMap = getNodesMap(nodes_in, src_tfg_llvm, dst_tfg_llvm, dst_tfg_asm);
+
+    var nodes = new vis.DataSet(nodes_in.map(function(node) {return {id:nodeMap[node.pc].idx, label:nodeMap[node.pc].label};}));
+    var edges = new vis.DataSet(edges_in.map(function(edge) {return {from:nodeMap[edge.from_pc].idx, to:nodeMap[edge.to_pc].idx, label:""/*edge.path1 + '\n\n' +edge.path2*/};}));
 
     var network = new vis.Network(document.getElementById('cfg'), {
         nodes: nodes,
@@ -94,7 +189,7 @@ function drawNetwork(cfg) {
                     }
                   }
             },
-            width: 3,
+            width: 6,
             widthConstraint: {
                 maximum: 300,
             }

ec1f3bfe328c4feacf89b66b1d244dc612220465
Sorav Bansal <sbansal@iitd.ac.in>
2023-05-09 20:07:41 +0530

    Added logic for obtaining scanview-report-url

--- eqchecker/media/scanview.js
+++ eqchecker/media/scanview.js
@@ -1,7 +1,7 @@
 const vscode = acquireVsCodeApi();
 
 var reportEl = document.getElementById("report");
-reportEl.innerHTML = "HELLO";
+reportEl.innerHTML = "";
 reportEl.style.fontSize = "16px";
 
 // Event listener for message from product graph webview
@@ -9,11 +9,13 @@ window.addEventListener('message', async event => {
     const message = event.data; // The JSON data our extension sent
 
     switch (message.command) {
-        case "scan": {
-          reportEl.innerHTML = message.report;
+        case "scanviewReport": {
+          //console.log(`scanviewReport received in scanview panel with url ${message.url}`);
+          reportEl.innerHTML = `<a href=${message.url}>${message.url}</a>`;
         }
         default: {
             break;
         }
     }
 });
+vscode.postMessage({command:"loaded"});
